e1abb6c34a3ea65ab6ab5332472f6aaabdecee31
Zachary Durr <zdurr22@gmail.com>
2020-09-20 23:17:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(board)
   counter = 0
   board.each do |index|
-    if index == "X" || "O"
+    if index == "X" || index == "O"
       counter += 1
     end
   end
